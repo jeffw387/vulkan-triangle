c778975590e07f9fc49651e8cf09ed7f42c36d29
Jeff Wright <jeff@teamjk.page>
2019-09-10 08:38:40 -0700

    mvp in vertex shader

--- src/main.rs
+++ src/main.rs
@@ -128,10 +128,12 @@ fn main() {
             src: "
 #version 450
 
-layout (location = 0) in vec2 position;
+layout (location = 0) in vec4 position;
+
+layout (set = 0, binding = 0) in mat4 mvp;
 
 void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = mvp * position;
 }"
         }
     }
